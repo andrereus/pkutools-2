f887232a0ea8e9f772fde81f46d65c8db90be0d5
Andre Reus <andrereus@users.noreply.github.com>
2024-01-16 11:15:51 +0100

    Remove comment

--- src/router/index.js
+++ src/router/index.js
@@ -67,9 +67,6 @@ const router = createRouter({
     {
       path: '/about',
       name: 'About',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/About.vue')
     },
     {
