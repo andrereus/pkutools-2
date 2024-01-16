c2e6c4d1867e520ac5ae54f299a10e803aade4a3
Andre Reus <andrereus@users.noreply.github.com>
2024-01-16 11:13:37 +0100

    Implement i18n

--- src/main.js
+++ src/main.js
@@ -9,8 +9,13 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+import { en as vEn, de as vDe, es as vEs, fr as vFr } from 'vuetify/locale'
 
 import { createI18n } from 'vue-i18n'
+import en from './locales/en.json'
+import de from './locales/de.json'
+import es from './locales/es.json'
+import fr from './locales/fr.json'
 
 const app = createApp(App)
 
@@ -23,10 +28,25 @@ const vuetify = createVuetify({
     sets: {
       mdi
     }
+  },
+  locale: {
+    locale: 'en',
+    fallback: 'en',
+    messages: {
+      en: vEn,
+      de: vDe,
+      es: vEs,
+      fr: vFr
+    }
   }
 })
 
-const i18n = createI18n({})
+const i18n = createI18n({
+  legacy: false,
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: { en, de, es, fr }
+})
 
 app.use(createPinia())
 app.use(router)
