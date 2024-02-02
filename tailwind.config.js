d9a037227eac9f6f1dc00cbe94827e35d8299c1c
Andre Reus <andrereus@users.noreply.github.com>
2024-02-02 20:09:20 +0100

    Remove daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
-import daisyui from 'daisyui'
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {}
   },
-  plugins: [daisyui],
+  plugins: [],
   prefix: 't-'
 }
