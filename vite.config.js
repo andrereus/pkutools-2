bd0fb4624cf9d7c0774cc8e3941c45fef7b12c45
Andre Reus <andrereus@users.noreply.github.com>
2024-02-03 23:00:50 +0100

    Add PWA and generate new icons

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,49 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'PKU Tools',
+        short_name: 'PKU Tools',
+        theme_color: '#3498db',
+        background_color: '#3498db',
+        display: 'standalone',
+        description: 'Tools and apps for PKU.',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
