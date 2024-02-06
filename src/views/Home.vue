321feb71f22cf08275da74ffd1805346aa4952c0
Andre Reus <andrereus@users.noreply.github.com>
2024-02-06 20:17:05 +0100

    Remove alert

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,6 @@
   <div>
     <v-row justify="center">
       <v-col cols="12" md="10" lg="8" xl="6">
-        <v-alert type="info" variant="tonal">
-          {{ $t('app.change-info') }}
-        </v-alert>
         <p v-if="!userIsAuthenticated" class="mt-5 mb-1">{{ $t('app.description') }}</p>
       </v-col>
     </v-row>
