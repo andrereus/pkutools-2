bf4c3e4b9d64d63294009f3cdeb1fe543d63ab07
Andre Reus <andrereus@users.noreply.github.com>
2024-02-15 14:15:52 +0100

    Safe area optimization

--- src/App.vue
+++ src/App.vue
@@ -418,6 +418,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.v-app {
+  padding-left: env(safe-area-inset-left) !important;
+  padding-top: env(safe-area-inset-top) !important;
+  padding-right: env(safe-area-inset-right) !important;
+  padding-bottom: env(safe-area-inset-bottom) !important;
+}
+
 .v-app-bar {
   padding-left: env(safe-area-inset-left) !important;
   padding-top: env(safe-area-inset-top) !important;
