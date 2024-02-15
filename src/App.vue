c793c9507deb7c6d061ac0cf2597f99b47199b24
Andre Reus <andrereus@users.noreply.github.com>
2024-02-15 14:38:33 +0100

    Safe area test 2

--- src/App.vue
+++ src/App.vue
@@ -430,7 +430,7 @@ export default {
   padding-bottom: env(safe-area-inset-bottom) !important;
 }
 
-#app {
+.v-application {
   padding-left: env(safe-area-inset-left) !important;
   padding-right: env(safe-area-inset-right) !important;
 }
