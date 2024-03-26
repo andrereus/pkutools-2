7d6d3113689b1db2454ad73e10524bc9e6c654bc
Andre Reus <andrereus@users.noreply.github.com>
2024-03-26 20:05:26 +0100

    Final update test

--- src/views/PheCalculator.vue
+++ src/views/PheCalculator.vue
@@ -2,7 +2,7 @@
   <div>
     <v-row justify="center">
       <v-col cols="12" md="10" lg="8" xl="6">
-        <h2 class="text-h5 mt-3">{{ $t('phe-calculator.title') }}.</h2>
+        <h2 class="text-h5 mt-3">{{ $t('phe-calculator.title') }}</h2>
       </v-col>
     </v-row>
 
