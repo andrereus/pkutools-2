5a6ad7018e552019d55750b64dd313f4184191dc
Andre Reus <andrereus@users.noreply.github.com>
2024-07-21 14:03:39 +0200

    Update headway button

--- src/App.vue
+++ src/App.vue
@@ -136,7 +136,7 @@
           >
             <button
               type="button"
-              class="t-relative t-rounded-full t-bg-white t-p-1 t-text-gray-400 hover:t-text-gray-500 focus:t-outline-none focus:t-ring-2 focus:t-ring-indigo-500 focus:t-ring-offset-2 headway dark:t-bg-gray-800 dark:t-text-gray-400 dark:hover:t-text-white"
+              class="headway t-relative t-rounded-full t-bg-white t-p-1 t-text-gray-400 hover:t-text-gray-500 focus:t-outline-none focus:t-ring-2 focus:t-ring-indigo-500 focus:t-ring-offset-2 dark:t-bg-gray-800 dark:t-text-gray-400 dark:hover:t-text-white"
             ></button>
 
             <Menu as="div" class="t-relative t-ml-3">
