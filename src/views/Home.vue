3cbd4c464cf92526805942336c96cb7d73470dbd
Andre Reus <andrereus@users.noreply.github.com>
2024-07-21 21:46:18 +0200

    Fix dark mode

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,13 +8,13 @@
         <nav class="t-flex t-space-x-4" aria-label="Tabs">
           <RouterLink
             to="/"
-            class="t-bg-gray-100 t-text-gray-700 t-rounded-md t-px-3 t-py-2 t-text-sm t-font-medium"
+            class="t-bg-gray-100 t-text-gray-700 t-rounded-md t-px-3 t-py-2 t-text-sm t-font-medium dark:t-bg-gray-700 dark:t-text-gray-300"
             aria-current="page"
             >{{ $t('phe-log.title') }}</RouterLink
           >
           <RouterLink
             to="/phe-diary"
-            class="t-text-gray-500 hover:t-text-gray-700 t-rounded-md t-px-3 t-py-2 t-text-sm t-font-medium"
+            class="t-text-gray-500 hover:t-text-gray-700 t-rounded-md t-px-3 t-py-2 t-text-sm t-font-medium dark:t-text-gray-300"
             >{{ $t('phe-diary.title') }}</RouterLink
           >
         </nav>
