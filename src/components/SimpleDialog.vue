588333368cea17f3f8d6c03ae71e2640bd8dffee
Andre Reus <andrereus@users.noreply.github.com>
2024-08-02 21:32:01 +0200

    Revert SimpleDialog back to simple

--- src/components/SimpleDialog.vue
+++ src/components/SimpleDialog.vue
@@ -48,14 +48,11 @@ defineEmits(['close', 'cancel'])
                   </div>
                 </div>
               </div>
-              <div
-                class="t-mt-5 sm:t-mt-6 sm:t-grid sm:t-grid-flow-row-dense sm:t-grid-cols-1 sm:t-gap-3"
-              >
+              <div class="t-mt-5 sm:t-mt-6">
                 <button
                   type="button"
-                  class="t-mt-3 t-inline-flex t-w-full t-justify-center t-rounded-md t-bg-white t-px-3 t-py-2 t-text-sm t-font-semibold t-text-gray-900 t-shadow-sm t-ring-1 t-ring-inset t-ring-gray-300 hover:t-bg-gray-50 sm:t-col-start-1 sm:t-mt-0"
+                  class="t-inline-flex t-w-full t-justify-center t-rounded-md t-bg-sky-500 t-px-3 t-py-2 t-text-sm t-font-semibold t-text-white t-shadow-sm hover:t-bg-sky-500 focus-visible:t-outline focus-visible:t-outline-2 focus-visible:t-outline-offset-2 focus-visible:t-outline-sky-500"
                   @click="$emit('cancel')"
-                  ref="cancelButtonRef"
                 >
                   {{ $t('common.cancel') }}
                 </button>
