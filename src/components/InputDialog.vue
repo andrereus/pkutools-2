1bbf5d75fbe70ce0a9fdfa8cacecb8aee5d55848
Andre Reus <andrereus@users.noreply.github.com>
2024-08-03 00:18:20 +0200

    Temporary fix

--- src/components/InputDialog.vue
+++ src/components/InputDialog.vue
@@ -22,7 +22,7 @@ defineEmits(['close', 'submit'])
 
       <div class="t-fixed t-inset-0 t-z-10 t-w-screen t-overflow-y-auto">
         <div
-          class="t-flex t-min-h-full t-items-end t-justify-center t-p-4 t-text-center sm:t-items-center sm:t-p-0"
+          class="t-flex t-min-h-full t-items-start t-justify-center t-p-4 t-text-center sm:t-items-center sm:t-p-0"
         >
           <TransitionChild
             as="template"
