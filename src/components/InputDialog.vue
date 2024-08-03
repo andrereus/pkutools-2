8d45c7fc53e0af73ad434e29d696cd5674fbdfa0
Andre Reus <andrereus@users.noreply.github.com>
2024-08-03 03:32:44 +0200

    Test 14

--- src/components/InputDialog.vue
+++ src/components/InputDialog.vue
@@ -25,7 +25,7 @@ const focus = ref(null)
 
       <div class="t-fixed t-inset-0 t-z-10 t-w-screen t-overflow-y-auto">
         <div
-          class="t-flex t-min-h-full t-items-start t-justify-center t-p-4 t-text-center sm:t-items-center sm:t-p-0"
+          class="t-flex t-min-h-full t-justify-center t-p-4 t-text-center t-items-center sm:t-p-0"
         >
           <TransitionChild
             as="template"
@@ -37,7 +37,7 @@ const focus = ref(null)
             leave-to="t-opacity-0 t-translate-y-4 sm:t-translate-y-0 sm:t-scale-95"
           >
             <DialogPanel
-              class="t-relative t-transform t-overflow-hidden t-rounded-lg t-bg-white dark:t-bg-gray-900 t-px-4 t-pb-4 t-pt-5 t-text-left t-shadow-xl t-transition-all sm:t-my-8 t-w-full t-max-w-md sm:t-max-w-lg sm:t-p-6"
+              class="t-relative t-transform t-overflow-auto t-rounded-lg t-bg-white dark:t-bg-gray-900 t-px-4 t-pb-4 t-pt-5 t-text-left t-shadow-xl t-transition-all sm:t-my-8 t-w-full t-max-w-md sm:t-max-w-lg sm:t-p-6"
             >
               <div>
                 <div>
