6637f4d90c1bbeaa930b6807053e41a2617ca1c2
Andre Reus <andrereus@users.noreply.github.com>
2024-12-11 16:59:50 +0100

    Refactor BarcodeScanner

--- src/views/BarcodeScanner.vue
+++ src/views/BarcodeScanner.vue
@@ -13,7 +13,7 @@
       <p v-if="error !== ''">{{ error }}</p>
 
       <QrcodeStream
-        v-if="open === true"
+        v-if="open"
         :track="paintBoundingBox"
         :formats="['ean_13', 'ean_8']"
         @camera-on="onReady"
@@ -22,7 +22,7 @@
       ></QrcodeStream>
     </SimpleDialog>
 
-    <div v-if="result !== null">
+    <div v-if="result">
       <img
         v-if="result.product.image_small_url"
         :src="result.product.image_small_url"
@@ -36,7 +36,7 @@
       <!-- Do not remove -->
       <p v-if="code !== ''" class="text-sm mb-6">Code: {{ code }}</p>
 
-      <div v-if="this.result.product.nutriments.proteins_100g">
+      <div v-if="result.product.nutriments.proteins_100g">
         <p class="text-xl mb-6">
           {{ result.product.nutriments.proteins_100g }}
           {{ result.product.nutriments.proteins_unit }}
@@ -61,7 +61,7 @@
         <PrimaryButton v-if="userIsAuthenticated" :text="$t('common.add')" @click="save" />
       </div>
 
-      <div v-if="!this.result.product.nutriments.proteins_100g" class="mb-6">
+      <div v-if="!result.product.nutriments.proteins_100g" class="mb-6">
         <p>{{ $t('barcode-scanner.no-protein') }}</p>
         <RouterLink to="/protein-calculator" class="text-sky-500">
           {{ $t('barcode-scanner.protein-link') }} <span aria-hidden="true">→</span>
@@ -75,9 +75,12 @@
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref, computed } from 'vue'
+import { useRouter } from 'vue-router'
+import { useI18n } from 'vue-i18n'
 import { useStore } from '../stores/index'
-import { getDatabase, ref, push } from 'firebase/database'
+import { getDatabase, ref as dbRef, push } from 'firebase/database'
 import { QrcodeStream } from 'vue-qrcode-reader'
 
 import PageHeader from '../components/PageHeader.vue'
@@ -86,141 +89,128 @@ import NumberInput from '../components/NumberInput.vue'
 import SimpleDialog from '../components/SimpleDialog.vue'
 import SelectMenu from '../components/SelectMenu.vue'
 
-export default {
-  components: {
-    QrcodeStream,
-    PageHeader,
-    PrimaryButton,
-    NumberInput,
-    SimpleDialog,
-    SelectMenu
-  },
-  data: () => ({
-    loaded: false,
-    open: false,
-    code: '',
-    error: '',
-    result: null,
-    weight: 100,
-    select: 'other'
-  }),
-  methods: {
-    paintBoundingBox(detectedCodes, ctx) {
-      for (const detectedCode of detectedCodes) {
-        const {
-          boundingBox: { x, y, width, height }
-        } = detectedCode
+const router = useRouter()
+const store = useStore()
+const { t } = useI18n()
+const dialog = ref(null)
 
-        ctx.lineWidth = 2
-        ctx.strokeStyle = '#007bff'
-        ctx.strokeRect(x, y, width, height)
-      }
-    },
-    onReady() {
-      this.loaded = true
-    },
-    onDetect(detectedCodes) {
-      this.code = detectedCodes[0].rawValue
+// Reactive state
+const loaded = ref(false)
+const open = ref(false)
+const code = ref('')
+const error = ref('')
+const result = ref(null)
+const weight = ref(100)
+const select = ref('other')
 
-      fetch('https://world.openfoodfacts.org/api/v2/product/' + this.code + '.json')
-        .then((response) => {
-          if (response.ok) {
-            return response.json()
-          }
-          throw new Error(response.status)
-        })
-        .then((result) => {
-          this.result = result
-        })
-        .catch((error) => {
-          alert(this.$t('barcode-scanner.error'))
-          console.log(error)
-        })
-      this.cancel()
-    },
-    onError(err) {
-      this.error = `[${err.name}]: `
+// Computed properties
+const type = computed(() => [
+  { title: t('protein-calculator.other'), value: 'other' },
+  { title: t('protein-calculator.meat'), value: 'meat' },
+  { title: t('protein-calculator.vegetable'), value: 'vegetable' },
+  { title: t('protein-calculator.fruit'), value: 'fruit' }
+])
 
-      if (err.name === 'NotAllowedError') {
-        this.error += 'you need to grant camera access permission'
-      } else if (err.name === 'NotFoundError') {
-        this.error += 'no camera on this device'
-      } else if (err.name === 'NotSupportedError') {
-        this.error += 'secure context required (HTTPS, localhost)'
-      } else if (err.name === 'NotReadableError') {
-        this.error += 'is the camera already in use?'
-      } else if (err.name === 'OverconstrainedError') {
-        this.error += 'installed cameras are not suitable'
-      } else if (err.name === 'StreamApiNotSupportedError') {
-        this.error += 'Stream API is not supported in this browser'
-      } else if (err.name === 'InsecureContextError') {
-        this.error +=
-          'Camera access is only permitted in secure context. Use HTTPS or localhost rather than HTTP.'
-      } else {
-        this.error += err.message
-      }
-    },
-    openDialog() {
-      this.open = true
-      this.$refs.dialog.openDialog()
-    },
-    cancel() {
-      this.loaded = false
-      this.open = false
-      this.$refs.dialog.closeDialog()
-    },
-    calculatePhe() {
-      return Math.round(
-        (this.weight * (this.result.product.nutriments.proteins_100g * this.factor)) / 100
-      )
-    },
-    save() {
-      const db = getDatabase()
-      push(ref(db, `${this.user.id}/pheLog`), {
-        name: this.result.product.product_name,
-        pheReference: Math.round(this.result.product.nutriments.proteins_100g * this.factor),
-        weight: Number(this.weight),
-        phe: this.calculatePhe()
-      })
-      this.$router.push('/')
-    }
-  },
-  computed: {
-    type() {
-      return [
-        { title: this.$t('protein-calculator.other'), value: 'other' },
-        { title: this.$t('protein-calculator.meat'), value: 'meat' },
-        { title: this.$t('protein-calculator.vegetable'), value: 'vegetable' },
-        { title: this.$t('protein-calculator.fruit'), value: 'fruit' }
-      ]
-    },
-    factor() {
-      if (this.select === 'fruit') {
-        return 27
-      } else if (this.select === 'vegetable') {
-        return 35
-      } else if (this.select === 'meat') {
-        return 46
-      } else {
-        return 50
-      }
-    },
-    userIsAuthenticated() {
-      const store = useStore()
-      return store.user !== null
-    },
-    user() {
-      const store = useStore()
-      return store.user
-    },
-    pheLog() {
-      const store = useStore()
-      return store.pheLog
-    },
-    settings() {
-      const store = useStore()
-      return store.settings
-    }
+const factor = computed(() => {
+  if (select.value === 'fruit') {
+    return 27
+  } else if (select.value === 'vegetable') {
+    return 35
+  } else if (select.value === 'meat') {
+    return 46
+  } else {
+    return 50
   }
+})
+
+const userIsAuthenticated = computed(() => store.user !== null)
+const user = computed(() => store.user)
+const pheLog = computed(() => store.pheLog)
+const settings = computed(() => store.settings)
+
+// Methods
+const paintBoundingBox = (detectedCodes, ctx) => {
+  for (const detectedCode of detectedCodes) {
+    const {
+      boundingBox: { x, y, width, height }
+    } = detectedCode
+
+    ctx.lineWidth = 2
+    ctx.strokeStyle = '#007bff'
+    ctx.strokeRect(x, y, width, height)
+  }
+}
+
+const onReady = () => {
+  loaded.value = true
+}
+
+const onDetect = async (detectedCodes) => {
+  code.value = detectedCodes[0].rawValue
+
+  try {
+    const response = await fetch(
+      'https://world.openfoodfacts.org/api/v2/product/' + code.value + '.json'
+    )
+    if (!response.ok) {
+      throw new Error(response.status)
+    }
+    result.value = await response.json()
+  } catch (error) {
+    alert(t('barcode-scanner.error'))
+    console.log(error)
+  }
+  cancel()
+}
+
+const onError = (err) => {
+  error.value = `[${err.name}]: `
+
+  if (err.name === 'NotAllowedError') {
+    error.value += 'you need to grant camera access permission'
+  } else if (err.name === 'NotFoundError') {
+    error.value += 'no camera on this device'
+  } else if (err.name === 'NotSupportedError') {
+    error.value += 'secure context required (HTTPS, localhost)'
+  } else if (err.name === 'NotReadableError') {
+    error.value += 'is the camera already in use?'
+  } else if (err.name === 'OverconstrainedError') {
+    error.value += 'installed cameras are not suitable'
+  } else if (err.name === 'StreamApiNotSupportedError') {
+    error.value += 'Stream API is not supported in this browser'
+  } else if (err.name === 'InsecureContextError') {
+    error.value +=
+      'Camera access is only permitted in secure context. Use HTTPS or localhost rather than HTTP.'
+  } else {
+    error.value += err.message
+  }
+}
+
+const openDialog = () => {
+  open.value = true
+  dialog.value.openDialog()
+}
+
+const cancel = () => {
+  loaded.value = false
+  open.value = false
+  dialog.value.closeDialog()
+}
+
+const calculatePhe = () => {
+  return Math.round(
+    (weight.value * (result.value.product.nutriments.proteins_100g * factor.value)) / 100
+  )
+}
+
+const save = () => {
+  const db = getDatabase()
+  push(dbRef(db, `${user.value.id}/pheLog`), {
+    name: result.value.product.product_name,
+    pheReference: Math.round(result.value.product.nutriments.proteins_100g * factor.value),
+    weight: Number(weight.value),
+    phe: calculatePhe()
+  })
+  router.push('/')
 }
 </script>
