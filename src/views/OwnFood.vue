5c0f7d9fe0e807649c61116f4fad57141bd69cd8
Andre Reus <andrereus@users.noreply.github.com>
2024-12-13 09:52:42 +0100

    Change limit

--- src/views/OwnFood.vue
+++ src/views/OwnFood.vue
@@ -95,7 +95,7 @@ const save = () => {
     })
   } else {
     if (
-      ownFood.value.length >= 500 &&
+      ownFood.value.length >= 200 &&
       settings.value.license !== import.meta.env.VITE_PKU_TOOLS_LICENSE_KEY
     ) {
       alert(t('own-food.limit'))
