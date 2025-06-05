a82cd4120022aa9ea821eb69fddafde71933aacd
Andre Reus <andrereus@users.noreply.github.com>
2025-06-05 21:24:09 +0200

    Optimize styles

--- src/views/EmailAuth.vue
+++ src/views/EmailAuth.vue
@@ -120,7 +120,7 @@ const resetPassword = async () => {
           <Tab v-slot="{ selected }">
             <button
               :class="[
-                'rounded-md px-3 py-2 text-sm font-medium dark:text-gray-300',
+                'rounded-md px-3 py-2 text-sm font-medium dark:text-gray-300 cursor-pointer',
                 selected
                   ? 'bg-black/5 dark:bg-white/15 text-gray-700'
                   : 'text-gray-500 hover:text-gray-700'
@@ -132,7 +132,7 @@ const resetPassword = async () => {
           <Tab v-slot="{ selected }">
             <button
               :class="[
-                'rounded-md px-3 py-2 text-sm font-medium dark:text-gray-300',
+                'rounded-md px-3 py-2 text-sm font-medium dark:text-gray-300 cursor-pointer',
                 selected
                   ? 'bg-black/5 dark:bg-white/15 text-gray-700'
                   : 'text-gray-500 hover:text-gray-700'
@@ -144,7 +144,7 @@ const resetPassword = async () => {
           <Tab v-slot="{ selected }">
             <button
               :class="[
-                'rounded-md px-3 py-2 text-sm font-medium dark:text-gray-300',
+                'rounded-md px-3 py-2 text-sm font-medium dark:text-gray-300 cursor-pointer',
                 selected
                   ? 'bg-black/5 dark:bg-white/15 text-gray-700'
                   : 'text-gray-500 hover:text-gray-700'
