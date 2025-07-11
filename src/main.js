7a04bc41a658a2cb0706bcff8888ed2307fa9439
Andre Reus <andrereus@users.noreply.github.com>
2025-07-11 10:06:21 +0200

    Add sentry

--- src/main.js
+++ src/main.js
@@ -18,6 +18,8 @@ import { getDatabase, connectDatabaseEmulator } from 'firebase/database'
 
 import VueApexCharts from 'vue3-apexcharts'
 
+import * as Sentry from '@sentry/vue'
+
 const i18n = createI18n({
   legacy: false,
   locale: localStorage.i18nCurrentLocale ? JSON.parse(localStorage.i18nCurrentLocale) : 'en',
@@ -48,6 +50,22 @@ if (import.meta.env.MODE === 'development') {
 }
 
 const app = createApp(App)
+
+Sentry.init({
+  app,
+  dsn: 'https://a5144f87af8f0035ddf31c7366915ea6@o4509648837214208.ingest.de.sentry.io/4509648842653776',
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+  integrations: [Sentry.browserTracingIntegration({ router }), Sentry.replayIntegration()],
+  // Tracing
+  tracesSampleRate: 1.0, // Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+})
 
 app.use(createPinia())
 app.use(router)
